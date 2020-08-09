674d98b0aac9a8124a53258f261f8cd616d89747
Michael Alexander <beefsack@gmail.com>
2020-08-09 10:24:07 +1000

    Fix build docker targets

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ docker build --target=api .
 docker build --target=web .
 docker build --target=websocket .
 docker build --target=acquire .
-docker build --target=lost_cities .
-docker build --target=age_of_war .
+docker build --target=lost-cities .
+docker build --target=age-of-war .
