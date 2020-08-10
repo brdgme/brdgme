02a8ca6f70fc128b07779f76f3082c77b61711bf
Michael Alexander <beefsack@gmail.com>
2020-08-10 19:51:35 +1000

    Use Buildkit for test.sh and build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+export DOCKER_BUILDKIT=1
 cd "$(dirname "$0")"
 docker build --target=api .
 docker build --target=web .
--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+export DOCKER_BUILDKIT=1
 cd "$(dirname "$0")"
 docker build --target=rust-test .
 docker build --target=go-test .
